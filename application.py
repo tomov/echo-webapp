23c8612d171b5aa77f3372ba16cb179a126629f8
Momchil Tomov <tomov90@gmail.com>
2013-08-08 02:44:04 -0700

    test notification

--- application.py
+++ application.py
@@ -72,6 +72,20 @@ def hello():
     create_db()
     return "Hello from Python yay!"
 
+@app.route("/test_notif")
+def test_notif():
+    # Send a notification
+    #token_hex1 = '04c11da985c7e9a615ddc039ce654b76e096db088602e71f8bbfc9fb6d59a91e' # rishi's phone
+    #token_hex1 = '884a19da5dc0a72d8aecb5ad6fe7ee2e49e9d8aacd618aedb785f067cb114de1' # rishi's phone 2
+    #token_hex1 = 'a40a3af5b674bc891614ecaf7a4d528150264e90a2dc1b16756148e14d41be64' # jacob's
+    #token_hex1 = '5093a4269fb4065bd70b6e23aed3f40b8978e1335cd0b889c9ed99c6c2d30631' # chris
+    #token_hex1 = 'a6f283a5eff9cd231efb1980558795a0443833d5d6470b61e972c8f786b9ae3f' # juan
+    token_hex1 = 'a951d8aba5ec3532edc6426583681e3749e2b71c9e1724219897382efd8154b0' # momchil
+    #payload = Payload(alert="Someone quoted you!", sound="default", badge=1)
+    payload = Payload(alert="This is a teeest notification. Please disregard.", sound="default", badge=69)
+    apns.gateway_server.send_notification(token_hex1, payload)
+    return "YES"
+
 #---------------------------------------
 #         Helper functions
 #----------------------------------------
@@ -529,7 +543,7 @@ def delete_quote(quoteId):
         if not user:
             raise ServerException(ErrorMessages.USER_NOT_FOUND, \
                 ServerException.ER_BAD_USER)
-            
+
         quote = Quote.query.filter_by(id = quoteId).first()
         if not quote or quote.deleted:
             raise ServerException(ErrorMessages.QUOTE_NOT_FOUND, \
